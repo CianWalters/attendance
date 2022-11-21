40f5e9fd05005dec4fcaeccca136c21e83d0693e
CianWalters <61118431+CianWalters@users.noreply.github.com>
2022-11-21 10:28:36 -0500

    Reuploaded - Started module 3 with the same git by error

--- includes/header.php
+++ includes/header.php
@@ -19,7 +19,7 @@ include_once 'includes/session.php';
     <title>Wally Bee Fitness - <?php echo $title ?></title>
 </head>
   <body style = "background: url(yellow-eraser.png)">
-  <nav class="navbar navbar-expand-lg navbar-dark bg-warning">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-info">
   <a class="navbar-brand" href="index.php">IT Conference</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
